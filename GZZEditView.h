497fd4788e93890fc61887090ee8a77dc67976e8
chenfeng <1397760231@qq.com>
2018-09-11 09:32:55 +0800

    Add files via upload
    初次构建

---
+++ GZZEditView.h
@@ -0,0 +1,13 @@
+//
+//  GZZEditView.h
+//  adadasd
+//
+//  Created by chenhaifeng on 2018/9/11.
+//  Copyright © 2018年 geely. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GZZEditView : UIView
+
+@end
---
+++ GZZEditView.m
@@ -0,0 +1,21 @@
+//
+//  GZZEditView.m
+//  adadasd
+//
+//  Created by chenhaifeng on 2018/9/11.
+//  Copyright © 2018年 geely. All rights reserved.
+//
+
+#import "GZZEditView.h"
+
+@implementation GZZEditView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
